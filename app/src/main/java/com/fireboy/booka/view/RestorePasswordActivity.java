be991f3b1136e06b076d89a9d12df553ef2256be
hugomartinromero <martin.romero.hugo@iescamas.es>
2025-05-06 10:29:00 +0200

    feat: login completo con diseño y validación básica

--- app/src/main/java/com/fireboy/booka/view/RestorePasswordActivity.java
+++ app/src/main/java/com/fireboy/booka/view/RestorePasswordActivity.java
@@ -1,6 +1,5 @@
 package com.fireboy.booka.view;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -11,18 +10,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.fireboy.booka.R;
 
-public class MainActivity extends AppCompatActivity {
+public class RestorePasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_restore_password);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
     }
 }
